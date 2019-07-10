4c3f3ae557f97c86e978ec806c8ee806aad7588d
exsquire <34141973+exsquire@users.noreply.github.com>
2019-07-10 14:11:24 -0700

    Control File

---
+++ cleanotype.R
@@ -0,0 +1,11 @@
+cat("Running Clean-o-Type...\n")
+
+#Walltime - ~100, user input based
+source("fstGen.R")
+
+#Walltime - 40.26
+source("proc_cross.R")
+
+#Walltime - 242.28 - with local data
+source("compute.R")
+
