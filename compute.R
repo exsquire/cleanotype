91b82f7008c12e8c3209b1a975cdecc43cfcd44f
exsquire <34141973+exsquire@users.noreply.github.com>
2019-07-09 18:03:38 -0700

    Saving recent changes - still incomplete

--- compute.R
+++ compute.R
@@ -12,17 +12,16 @@ library(data.table)
 library(kableExtra)
 
 test_local = TRUE
-use_ncores = 7
+use_ncores = 6
 #---------------------------------------------
-cat("Running Clean-o-type...\n")
+cat("Running Clean-o-type...")
 cat("\nCalling script: compute.R\n")
-cat("Default cores set to:", use_ncores)
+cat("\nDefault cores set to:", use_ncores)
 #---------------------------------------------
 pathIn <- list(
   CrossRaw = list.files(path = "../input/",pattern = "cross", full.names = T, ignore.case = T),
   CrossPass1 = list.files(path = "../output/objects/",pattern = "pass1.rds", full.names = T)
 )
-
 
 #Check
 if(!all(sapply(pathIn, function(x) length(x) > 0))){
@@ -36,19 +35,34 @@ if(!all(sapply(pathIn, function(x) length(x) > 0))){
 cross_Raw <- readRDS(pathIn[[1]])
 cross_pass1 <- readRDS(pathIn[[2]])
 #Generate GPR_Raw from crossRaw
-cat("\nCalculating genotype probabilities.\nThis may take a while...")
-gpr_Raw <- calc_genoprob(cross_Raw, cores = use_ncores)
-cat("Finished.\n")
+
+if(test_local){
+  cat("\nLocal Test: Reading in gpr_Raw.\nPlease wait...")
+  gpr_Raw <- readRDS("../testDat/DOWL_gpr_preClean.rds")
+  cat("Finished.\n")
+}else{
+  cat("\nCalculating genotype probabilities.\nPlease wait...")
+  gpr_Raw <- calc_genoprob(cross_Raw, cores = use_ncores)
+  saveRDS(gpr_Raw, file = "../output/objects/gpr_Raw.rds")
+  cat("Finished.\n")
+}
+
 #----------------------------------------------
 
 #Generate e - most are largely negative - few are above 2, those 
 #are what we want to scrutinize as they represent a large
 #departure from what the multiparent gpr suggests
-cat("\nCalculating genotyping error LOD scores...")
-cat("Source: Lincoln SE, Lander ES (1992)\nSystematic detection of errors in genetic linkage data.\nGenomics 14:604-610.")
-e <- calc_errorlod(cross_Raw, gpr_Raw, cores = use_ncores)
-e <- do.call("cbind", e)
-saveRDS(e, file = "../output/objects/e.rds")
+cat("\nCalculating genotyping error LOD scores...\n")
+cat("Source: Lincoln SE, Lander ES (1992)\nSystematic detection of errors in genetic linkage data.\nGenomics 14:604–610.")
+if(test_local){
+  e <- readRDS("../testDat/errorLODbase_e.rds")
+  e <- do.call("cbind", e)
+}else{
+  e <- calc_errorlod(cross_Raw, gpr_Raw, cores = use_ncores)
+  e <- do.call("cbind", e)
+  saveRDS(e, file = "../output/objects/e.rds")
+}
+
 
 #Examine samps with error LOD greater than 2
 #Error_ind is the significant eLOD per sample
@@ -118,9 +132,10 @@ eMarLOD <- ggplot(errmarDF,
 
 eMarLOD %>% ggexport(filename = "../output/plots/eLODMark_Diag.pdf")
 #------------------------------------
-
+cat("\nGenerating Marker Tri-plot Diagnostic...")
 gf_mar <- t(apply(g, 2, function(a) table(factor(a, 1:3))/sum(a != 0)))
 gn_mar <- t(apply(g, 2, function(a) table(factor(a, 1:3))))
+
 
 pdf("../output/plots/MarkTriPlot.pdf")
 par(mfrow=c(2,2), mar=c(0.6, 0.6, 2.6, 0.6))
@@ -132,9 +147,21 @@ for(i in 1:4) {
   tripoints(c((1-i/8)^2, 2*i/8*(1-i/8), (i/8)^2), pch=21, bg="violetred")
 }
 dev.off()
+cat("Finished.\n")
 #------------------------------------
+if(test_local){
+  cat("\nLocal Test: Reading in m.\nPlease wait...")
+  m <- readRDS("../testDat/XObase_m.rds")
+  cat("Finished.\n")
+}else{
+  cat("\nGenerating and Exporting Maximal Marginal Probability Genotypes...\n")
+  m <- maxmarg(gpr_Raw, minprob = 0.5, cores = use_ncores)
+  saveRDS(m, file = "../output/objects/m.rds")
+  cat("Finished.\n")
+}
 
-m <- maxmarg(gpr_Raw, minprob = 0.5, cores = use_ncores)
+cat("\nGenerating Crossover Diagnostic...")
+cat("Finished.\n")
 nxo <- count_xo(m, cores = 4)
 totxo <- rowSums(nxo)
 
@@ -163,19 +190,24 @@ xoPlot <- ggplot(xoDF,
   theme_bw()
 
 xoPlot %>% ggexport(filename = "../output/plots/xoPlot.pdf") 
-
-
+cat("Finished.\n")
+#-----------------------------------
 #Kable
 #Kick out a kable of percent missing
-percMissDF <- readRDS("../output/objects/percMiss_SampDF.rds")
-percMissDF$xo <- totxo[match(percMissDF$Mouse,names(totxo))]
+percMissDF <- readRDS("../output/objects/percMiss_SampDF.rds") %>%
+  arrange(desc(val))
+rownames(percMissDF) <- gsub(" .*$","",percMissDF$samp)
+
+percMissDF$xo <- totxo[match(rownames(percMissDF),names(totxo))]
+percMissDF$mouse <- rownames(percMissDF)
+
 miss_thresh <- mean(percMissDF$val) + 3*sd(percMissDF$val)
 xo_thresh <- mean(percMissDF$xo) + 3*sd(percMissDF$xo)
 
 
 #Kable presents the mice in descending order of missing genotype data - the rows were the percent missing is greater than 3 sd from the mean are bolded and highlighted with a red background.
-percMissDF[,c(1,4,5), drop = F]%>% 
-  kable(col.names = c("Mouse","Missing (%)","Crossovers")) %>% 
+percMissDF[,c(3,4), drop = F]%>% 
+  kable(col.names = c("Missing (%)","Crossovers")) %>% 
   kable_styling(bootstrap_options = c("striped"), 
                 full_width = F)%>%
   row_spec(which(percMissDF$val >= miss_thresh), bold = T,
@@ -184,22 +216,60 @@ percMissDF[,c(1,4,5), drop = F]%>%
   row_spec(which(percMissDF$xo >= xo_thresh), bold = T,
            color = "white",
            background = "Red") %>%
-  save_kable(file = "../output/plots/missXO_kable.pdf")
+  save_kable(file = "../output/objects/missXO_kable.png")
+cat("Finished.\n")
 
 #Kick out a csv
 #write.csv(round(percMissDF[,3, drop = F],2), file = "PercMiss.csv")
 
 #-----------------------------------
+#generate cross_Clean 
+cat("\nFiltering 'Cross_Pass1' for markers with > 5% Error Rate\n")
+cross_Clean <- drop_markers(cross_pass1, names(error_mar)[error_mar > 5])
+cat("Exporting 'Cross_Clean' object...")
+#Make gpr_CLEAN
+if(test_local){
+  cat("\nLocal Test: Reading in gpr_Clean.\nPlease wait...")
+  gpr_Clean <- readRDS("../testDat/DOWL_gpr_clean.rds")
+  cat("Finished.\n")
+}else{
+  cat("\nCalculating cleaned genotype probabilities.\nPlease wait...")
+  gpr_Clean <- calc_genoprob(cross_Clean, cores = use_ncores)
+  saveRDS(gpr_Clean, file = "../output/objects/gpr_Clean.rds")
+  cat("Finished.\n")
+}
 
+cat("\nGenerating and Exporting Multiparent-predicted Genotypes...\n")
 snpg <- predict_snpgeno(cross_Clean, m, cores = use_ncores)
 snpg <- do.call("cbind", snpg)
 saveRDS(snpg, file = "../output/objects/snpg.rds")
+cat("Finished.\n")
 
-#generate cross_Clean 
 
-#Make gpr_CLEAN
+
+#For tomorrow - make the markers flushed using qtl2 function or for loop
+
+#Make gprs flush
+gpr_Clean <- gpr_Clean[ind_ids(cross_Clean),]
+gpr_Raw <- gpr_Raw[ind_ids(cross_Clean),]
 
 #Find areas of largest disagreement
+prdiff <- vector("list",length(gpr_Raw))
+for(i in seq_along(prdiff)){
+  prdiff[[i]] <- apply(abs(gpr_Raw[[i]] - gpr_Clean[[i]]),c(1,3),sum)
+}
+names(prdiff) <- names(gpr_Raw)
+
+#----------------------------------------
+#Differences take values between 0 and 2. 2 == completely different
+#Show # of markers x samples per chromosome with differences > 1.5 (arbitrary, ~75% different)
+checkDiff_mark <- sapply(prdiff, function(d) sum(d > 1.5))
+#Show # of samples per chromosome with at least 5 markers with an absolute difference > 1.5
+checkDiff_samp <- sapply(prdiff, function(d) sum(rowSums(d > 1.5) > 5))
+saveRDS(checkDiff_mark, file = "../output/objects/checkDiff_mark.rds")
+saveRDS(checkDiff_samp, file = "../output/objects/checkDiff_samp.rds")
+
+#Perform Marker and Sample Specific Diagnostics in another script
 
 invisible(gc())
 cat("\ncompute.R: Status: Complete")
